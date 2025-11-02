03f783eec82be3f9b04b85cf45b735c1ca775273
Aviran Giat <aviran@g.jct.ac.il>
2025-11-02 18:17:15 +0200

    Added Spring Boot application entry point

---
+++ src/main/java/com/dropit/delivery/api/DeliveryApiApplication.java
@@ -0,0 +1,14 @@
+package com.dropit.delivery.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application.
+ */
+@SpringBootApplication
+public class DeliveryApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryApiApplication.class, args);
+	}
+}
